d3aa1f31c08083b57f0dc3806a72e611b5a44252
Winckler <au534747@post.au.dk>
2019-08-29 18:37:09 +0200

    added accumulator

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,33 +9,33 @@ namespace Calculator
 {
     public class Calculator
     {
+        private static double result;
         public double Add(double a, double b)
         {
-            double result = a + b;
+            result = a + b;
             return result;
         }
 
         public double Subtract(double a, double b)
         {
-            double result = a - b;
+            result = a - b;
             return result;
         }
 
         public double Multiply(double a, double b)
         {
-            double result = a * b;
+            result = a * b;
             return result;
         }
 
         public double Power(double x, double exp)
         {
-            double result = Math.Pow(x, exp);
+            result = Math.Pow(x, exp);
             return result;
         }
 
         public double Divide(double divident, double divider)
         {
-            double result = 0;
 
             if (divider != 0)
             {
@@ -46,5 +47,19 @@ namespace Calculator
                 throw new InvalidOperationException("Divide by 0 error");
             }
         }
+
+        public double Accumulator
+        {
+
+            get
+            {
+                double resultValue = result;
+                return resultValue;
+            }
+            //private omitted
+
+        }
+
+
     }
 }
