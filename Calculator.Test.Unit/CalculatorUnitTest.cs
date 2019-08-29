283e1c133e5f39171902f3999365adc5f27363dd
Morten Filt <201600311@post.au.dk>
2019-08-29 16:28:00 +0200

    Added Calculator project with simplified unit test

---
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        private Calculator uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            uut = new Calculator();
+        }
+
+        [Test]
+        public void Add_TwoAndThree_Return5()
+        {
+            // Arrange
+            double result = 0;
+            // Act
+            result = uut.Add(2, 3);
+            // Assert
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Subtract_ThreeAndTwo_Return1()
+        {
+            // Arrange
+            double result = 0;
+            // Act
+            result = uut.Subtract(3, 2);
+            // Assert
+            Assert.That(result,Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Multiply_FourAndThree_Return12()
+        {
+            // Arrange
+            double result = 0;
+            // Act
+            result = uut.Multiply(4, 3);
+            // Assert
+            Assert.That(result,Is.EqualTo(12));
+        }
+
+        [Test]
+        public void Power_TwoIntThePowerOfThree_Return8()
+        {
+            // Arrange
+            double result = 0;
+            // Act
+            result = uut.Power(2, 3);
+            // Assert
+            Assert.That(result,Is.EqualTo(8));
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            System.Console.WriteLine("Starting test of calculator\n");
+
+            Calculator calc = new Calculator();
+            double resultOfTest = 0.0;
+            System.Console.WriteLine("\nCommencing addition test");
+            // Addition test
+            // 2 positive numbers
+            resultOfTest = calc.Add(2, 2);
+            if (resultOfTest == 4.0)
+            {
+                System.Console.WriteLine($"Performing, 2 + 2 expecting 4: \t Result of test {resultOfTest} \t - OK");
+            }
+            else
+            {
+                System.Console.WriteLine($"Performing, 2 + 2 expecting 4: \t Result of test {resultOfTest} \t - Not OK");
+            }
+
+            System.Console.WriteLine("\nCommencing subtraction test");
+            // Subtraction test
+            // 2 positive numbers
+            resultOfTest = calc.Subtract(2, 2);
+            if (resultOfTest == 0)
+            {
+                System.Console.WriteLine($"Performing, 2 - 2 expecting 0: \t Result of test {resultOfTest} \t - OK");
+            }
+            else
+            {
+                System.Console.WriteLine($"Performing, 2 - 2 expecting 0: \t Result of test {resultOfTest} \t - Not OK");
+            }
+
+            System.Console.WriteLine("\nCommencing multiplication test");
+            // Multiplication test
+            // 2 positive numbers
+            resultOfTest = calc.Multiply(2, 2);
+            if (resultOfTest == 4.0)
+            {
+                System.Console.WriteLine($"Performing, 2 * 2 expecting 4: \t Result of test {resultOfTest} \t - OK");
+            }
+            else
+            {
+                System.Console.WriteLine($"Performing, 2 * 2 expecting 4: \t Result of test {resultOfTest} \t - Not OK");
+            }
+
+            System.Console.WriteLine("\nCommencing power test");
+            // Power test
+            // 2 positive numbers
+            resultOfTest = calc.Power(2, 2);
+            if (resultOfTest == 4.0)
+            {
+                System.Console.WriteLine($"Performing, 2 ^ 2 expecting 4: \t Result of test {resultOfTest} \t - OK");
+            }
+            else
+            {
+                System.Console.WriteLine($"Performing, 2 ^ 2 expecting 4: \t Result of test {resultOfTest} \t - Not OK");
+            }
+            */
+        }
+    }
+}
