2dba2022c45bd7a0c994b92addd14272a2cdadbd
Morten Filt <201600311@post.au.dk>
2019-09-05 12:32:41 +0200

    Jenkins test part 2

--- Calculator.Test.Unit/CalculatorUnitTest.cs
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -120,7 +120,7 @@ namespace Calculator.Test.Unit
 
         [TestCase(-4, -2, 2)]
         [TestCase(-2, -4, 0.5)]
-        [TestCase(-4, -4, -1)]
+        [TestCase(-4, -4, 1)]
         [TestCase(-0, -4, 0)]
         public void Divide_TwoNegativeNumbers_ResultIsCorrect(double divident, double divider, double result)
         {
